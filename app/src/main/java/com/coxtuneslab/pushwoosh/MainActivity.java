b5cbd2b1d92bd812d1a395376d3e31c8a78a2b94
Abir-CSE <abircoxsbazar@gmail.com>
2020-08-18 14:31:06 +0600

    intial commit

---
+++ app/src/main/java/com/coxtuneslab/pushwoosh/MainActivity.java
@@ -0,0 +1,18 @@
+package com.coxtuneslab.pushwoosh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pushwoosh.Pushwoosh;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Pushwoosh.getInstance().registerForPushNotifications();
+    }
+}
